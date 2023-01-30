71fdc1ce51157c1f80741bb57cd43c6d769b2f2a
Razeral <ng.shangru@gmail.com>
2023-01-30 22:55:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ UOBMerchants.MerchantList.value
 
 title = st.text_input('Merchant Search', 'Giant')
 exclusionList = title in UOBMerchants.MerchantList.value
-matchedList = [j for i, j in enumerate(UOBMerchants.MerchantList.value)]
+matchedList = [j for j UOBMerchants.MerchantList.value]
 st.write('The current movie title is', title)
 
 
