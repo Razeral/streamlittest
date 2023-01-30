7185e7b30e6c006d8732c0c71001c8c0cb85d162
Razeral <ng.shangru@gmail.com>
2023-01-30 22:32:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from constants import (UOBMerchants)
+
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
@@ -34,3 +36,5 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+UOBMerchants.MerchantList.value
